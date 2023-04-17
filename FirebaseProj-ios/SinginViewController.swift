43c7da8e91d821ee99fca81e8b9c378da834f01e
김신승 <94797657+kimshinseung@users.noreply.github.com>
2023-04-17 20:51:11 +0900

    Signin

---
+++ FirebaseProj-ios/SinginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SinginViewController.swift
+//  FirebaseProj-ios
+//
+//  Created by 김신승 on 2023/04/16.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SinginViewController: UIViewController {
+    
+    @IBOutlet weak var EmailTextField: UITextField!
+    
+    @IBOutlet weak var PwTextField: UITextField!
+    
+    //뒤로 나가는 코드
+    @IBAction func ExitButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func SingInButton(_ sender: UIButton) {
+        Auth.auth().createUser(withEmail: EmailTextField.text!, password: PwTextField.text!){(user,error) in
+            if user != nil{
+                self.showToast(message: "회원가입에 성공했습니다.")
+            }
+            else{
+                self.showToast(message: "회원가입에 실패했습니다.")
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+    
+}
--- FirebaseProj-ios/ViewController.swift
+++ FirebaseProj-ios/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var pwTextField: UITextField!
     
+    //로그인버튼, 파이어베이스에 정보가 있으면 성공, 없으면 실패
     @IBAction func loginButtonTouched(_ sender: UIButton) {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: pwTextField.text!) { (user, error) in
                     if user != nil{
@@ -23,11 +24,22 @@ class ViewController: UIViewController {
                         print("login fail")
                     }
               }
-        
     }
+    //회원가입하는 버튼
+    @IBAction func SinginButtonTouched(_ sender: UIButton) {
+        let newVC = self.storyboard?.instantiateViewController(identifier: "SignInBoard")
+                newVC?.modalTransitionStyle = .coverVertical
+                newVC?.modalPresentationStyle = .automatic
+                self.present(newVC!, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if Auth.auth().currentUser != nil{
+            emailTextField.text = "이미 로그인 된 상태입니다."
+            
+        }
     }
 
 
